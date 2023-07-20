efdec955727a2f42a99b738a0c9a8a8f0d4dc5c9
hmckeeman <hmckeeman00@gmail.com>
2023-07-20 11:34:23 -0300

    changed file names and fixed variables for applicant clarity

--- contracts/applicantContracts/register.sol
+++ contracts/applicantContracts/register.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../universityContracts/admissionsOffice.sol";
+import "../universityContracts/admissions.sol";
 
 contract Register {
     address payable public admissionsOffice;
     address public applicant;
 
-    constructor(address payable _admissionsOffice) {
-        admissionsOffice = _admissionsOffice;
+    constructor(address payable _admissions) {
+        admissionsOffice = _admissions;
         applicant = msg.sender;
     }
 
@@ -17,18 +17,18 @@ contract Register {
         _;
     }
 
-    function registerWithAdmissionsOffice() external payable {
+    function registerWithAdmissions() external payable {
         require(admissionsOffice != address(0), "AdmissionsOffice address not set");
         require(msg.value >= 10 wei, "Insufficient payment");
 
         (bool success, ) = admissionsOffice.call{value: 10 wei}("");
         require(success, "ETH transfer to AdmissionsOffice failed");
 
-        AdmissionsOffice(admissionsOffice).addApplicant(applicant);
+        Admissions(admissionsOffice).addApplicant(applicant);
     }
 
     function getAssignedOfficer() external view onlyApplicant returns (address) {
         // Retrieve the assigned officer for the contract deployer (applicant)
-        return AdmissionsOffice(admissionsOffice).getAssignedOfficer(applicant);
+        return Admissions(admissionsOffice).getAssignedOfficer(applicant);
     }
 }
--- contracts/universityContracts/admissions.sol
+++ contracts/universityContracts/admissions.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract AdmissionsOffice {
+contract Admissions {
     address[] private unassignedApplicants;
     address[] private assignedApplicants;
     address[] private acceptedStudents;
