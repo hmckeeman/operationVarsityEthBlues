f7504eeba878de21afe4d1d12fd0c1aefb2cd183
hmckeeman <hmckeeman00@gmail.com>
2023-07-27 12:51:50 -0300

    simplified applicant.sol

--- contracts/applicantContracts/applicant.sol
+++ contracts/applicantContracts/applicant.sol
@@ -3,34 +3,29 @@ pragma solidity ^0.8.0;
 
 import "../universityContracts/admissions.sol";
 
-contract Register {
+contract Applicant {
+    
     address public admissionsOffice;
     address public applicant;
-    address public registerAddress;
     bool public decisionReceived; // New variable to track decision status
     string public admissionDecision; // New variable to store the admission decision
 
     constructor(address _admissions) {
         admissionsOffice = _admissions;
-        applicant = address(this);
+        applicant = address(this); // Set applicant to the address of the caller (the deployer of the contract)
 
         Admissions(admissionsOffice).addApplicant(applicant);
     }
 
-    modifier onlyApplicant() {
-        require(msg.sender == registerAddress, "Only the applicant can call this function");
-        _;
-    }
-
-    function getAssignedOfficer() external view onlyApplicant returns (address) {
-        return Admissions(admissionsOffice).getAssignedOfficer(registerAddress);
+    function getAssignedOfficer() external view returns (address) {
+        return Admissions(admissionsOffice).getAssignedOfficer(applicant);
     }
 
     function isApplicantRegistered() external view returns (bool) {
-        return Admissions(admissionsOffice).isApplicantRegistered(registerAddress);
+        return Admissions(admissionsOffice).isApplicantRegistered(applicant);
     }
 
-    function receiveDecision(string calldata decision) external onlyApplicant {
+    function receiveDecision(string calldata decision) external {
         require(!decisionReceived, "Decision has already been received");
         decisionReceived = true;
         admissionDecision = decision;
