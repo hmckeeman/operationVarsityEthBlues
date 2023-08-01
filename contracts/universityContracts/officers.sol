15978c314d6b9e0154ddec1c1aaa4b9c999b2a0e
hmckeeman <hmckeeman00@gmail.com>
2023-08-01 17:46:35 -0300

    cleaned up all the mappings into applicantsData and fixed communication bug in makeDecision

--- contracts/universityContracts/officers.sol
+++ contracts/universityContracts/officers.sol
@@ -18,45 +18,35 @@ contract Officer is IERC721Receiver {
     address private officer;
     address private admissionsContract;
     uint256 private totalAssignedApplicants;
-    mapping(uint256 => address) private tokenIdToApplicant;
-    mapping(address => ApplicantData) private applications;
-    mapping(address => uint256) private applicantContractToTokenId; // Mapping to store the token ID based on the applicant contract address
+    address private deployer; // New variable to store the deployer's address
+    mapping(address => ApplicantData) private applicantsData;
 
     constructor(address _admissionsContract) {
-        officer = msg.sender;
+        deployer = msg.sender; // Set the deployer address when deploying the contract
         admissionsContract = _admissionsContract;
-    }
-
-    modifier onlyOfficer() {
-        require(msg.sender == officer, "Only the officer can call this function");
-        _;
     }
 
     function onERC721Received(address, address from, uint256 tokenId, bytes calldata) external override returns (bytes4) {
         require(totalAssignedApplicants < type(uint256).max, "Maximum number of applicants reached");
-        tokenIdToApplicant[tokenId] = from;
 
         Application applicationContract = Application(msg.sender);
         Application.ApplicantData memory applicant = applicationContract.getApplication(tokenId);
 
-        applications[from] = ApplicantData(applicant.name, applicant.university, applicant.ipfsLink, false, "");
-        applicantContractToTokenId[from] = tokenId; // Save the token ID based on the applicant contract address
+        applicantsData[from] = ApplicantData(applicant.name, applicant.university, applicant.ipfsLink, false, "");
 
         totalAssignedApplicants++;
         return this.onERC721Received.selector;
     }
 
-    function getAssignedApplicants() onlyOfficer external view returns (address[] memory) {
+    function getAssignedApplicants() external view returns (address[] memory) {
         address[] memory applicantsForOfficer = Admissions(admissionsContract).getApplicantsForOfficer(address(this));
         return applicantsForOfficer;
     }
 
-    function viewApplication(address applicantContract) onlyOfficer external view returns (string memory, string memory, string memory, bool, string memory) {
+    function viewApplicant(address applicantContract) external view returns (string memory, string memory, string memory, bool, string memory) {
         require(isApplicantAssigned(applicantContract), "You can only view applications of applicants you have been assigned to");
 
-        uint256 tokenId = applicantContractToTokenId[applicantContract]; // Get the token ID from the mapping
-        address applicant = tokenIdToApplicant[tokenId]; // Get the applicant's address from the token ID mapping
-        ApplicantData storage applicantData = applications[applicant];
+        ApplicantData memory applicantData = applicantsData[applicantContract];
         if (bytes(applicantData.name).length != 0) {
             return (
                 applicantData.name,
@@ -70,19 +60,16 @@ contract Officer is IERC721Receiver {
         }
     }
 
-    function makeDecision(address applicant, string calldata decision) onlyOfficer external {
-        require(isApplicantAssigned(applicant), "You can only approve applications of applicants you have been assigned to");
+    function makeDecision(address applicantContract, string calldata decision) external {
+        require(isApplicantAssigned(applicantContract), "You can only approve applications of applicants you have been assigned to");
         require(bytes(decision).length > 0, "Decision cannot be empty");
-        ApplicantData storage applicantData = applications[applicant];
+
+        ApplicantData storage applicantData = applicantsData[applicantContract];
         require(!applicantData.decisionMade, "Decision already made for this applicant");
         applicantData.decisionMade = true;
         applicantData.decision = decision;
 
-        uint256 tokenId = applicantContractToTokenId[applicant];
-        require(tokenId != 0, "Token ID not found for the applicant");
-        address applicationContractAddress = tokenIdToApplicant[tokenId];
-        require(applicationContractAddress != address(0), "Applicant contract not found for the applicant");
-        Applicant(applicationContractAddress).receiveDecision(decision); // Call the receiveDecision function of the Applicant contract
+        Applicant(applicantContract).receiveDecision(decision); // Call the receiveDecision function of the Applicant contract
     }
 
     function isApplicantAssigned(address applicantContract) internal view returns (bool) {
