3b92645ecc47aadb3164a0fa37fa0f5b9adb189c
hmckeeman <hmckeeman00@gmail.com>
2023-08-05 15:08:59 -0300

    autmated the grantOfficerApproval function by moving it to the assignAdmissionsOfficer function in admissions.sol

--- contracts/applicantContracts/applicant.sol
+++ contracts/applicantContracts/applicant.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.0;
 import "../universityContracts/admissions.sol";
 
 contract Applicant {
-    address public admissionsOffice;
+    address private deployer;
+    address public admissionsContract;
     address public applicant;
     bool public decisionReceived; // New variable to track decision status
     string public admissionDecision; // New variable to store the admission decision
@@ -19,18 +20,21 @@ contract Applicant {
 
     constructor(address _admissions) {
         contractDeployer = msg.sender; // Set the contract deployer's address
-        admissionsOffice = _admissions;
+        admissionsContract = _admissions;
         applicant = address(this); // Set applicant to the address of the caller (the deployer of the contract)
 
-        Admissions(admissionsOffice).addApplicant(applicant);
+        Admissions(admissionsContract).addApplicant(applicant);
     }
 
+    // Event to notify the officer approval
+    event OfficerApprovalGranted(address officerContract);
+
     function getAssignedOfficer() external view onlyAuthorized returns (address) {
-        return Admissions(admissionsOffice).getAssignedOfficer(applicant);
+        return Admissions(admissionsContract).getAssignedOfficer(applicant);
     }
 
     function isApplicantRegistered() external view onlyAuthorized returns (bool) {
-        return Admissions(admissionsOffice).isApplicant(applicant);
+        return Admissions(admissionsContract).isApplicant(applicant);
     }
 
     function receiveDecision(string calldata decision) external onlyAuthorized {
@@ -39,7 +43,9 @@ contract Applicant {
         admissionDecision = decision;
     }
 
-    function grantOfficerApproval(address _officerContract) external onlyAuthorized {
+    function grantOfficerApproval(address _officerContract) external {
         officerContract = _officerContract;
+        emit OfficerApprovalGranted(_officerContract);
     }
+
 }
