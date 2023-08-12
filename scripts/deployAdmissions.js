a8f43fc8070b227373830065ed7333ad9bd2f73b
hmckeeman <hmckeeman00@gmail.com>
2023-08-12 16:13:35 -0300

    worked on trying to trouble shoot bugs in deployAdmissions.sol... to be continued

--- scripts/deployAdmissions.js
+++ scripts/deployAdmissions.js
@@ -1,26 +1,40 @@
-require('dotenv').config(); // Load environment variables from .env file
-
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { abi, evm } = require('../build/contracts/Admissions.json'); // Make sure the path is correct
+const { abi, evm } = require('../build/contracts/Admissions.json');
+
+require('dotenv').config();
+
+const mnemonic = process.env.MNEMONIC;
+const ganacheUrl = 'http://localhost:8545'; // Change to the correct Ganache URL
 
 const provider = new HDWalletProvider({
-  mnemonic: process.env.MNEMONIC,
-  providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+  mnemonic: {
+    phrase: mnemonic
+  },
+  providerOrUrl: ganacheUrl
 });
 
 const web3 = new Web3(provider);
 
-const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
+(async () => {
+  try {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Deploying from account:', accounts[0]);
 
-  console.log('Attempting to deploy from account:', accounts[0]);
+    const contract = new web3.eth.Contract(abi);
 
-  const result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object })
-    .send({ gas: '1000000', from: accounts[0] });
+    const result = await contract
+      .deploy({
+        data: evm.bytecode.object,
+        arguments: [1000] // Replace with constructor arguments if needed
+      })
+      .send({
+        gas: '5000000',
+        from: accounts[0]
+      });
 
-  console.log('Contract deployed to:', result.options.address);
-};
-
-deploy();
+    console.log('Contract deployed to:', result.options.address);
+  } catch (error) {
+    console.error('Error deploying contract:', error);
+  }
+})();
