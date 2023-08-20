21f0f133c84c6174a2456323bb15f17ea9387f79
hmckeeman <hmckeeman00@gmail.com>
2023-08-20 13:38:05 -0300

    added script for assignOfficers.js

---
+++ scripts/assignOfficers.js
@@ -0,0 +1,27 @@
+const Admissions = artifacts.require("Admissions");
+
+module.exports = async function(callback) {
+    try {
+        // Fetch the deployed Admissions contract instance
+        const admissions = await Admissions.deployed();
+
+        // Check the number of unassigned applicants before the assignment
+        let {0: unassignedBefore} = await admissions.getUnassignedApplicants();
+        console.log(`Number of unassigned applicants before assignment: ${unassignedBefore}`);
+
+        // Assign admissions officers to the unassigned applicants
+        await admissions.assignAdmissionsOfficer();
+
+        // Check the number of unassigned applicants after the assignment
+        let {0: unassignedAfter} = await admissions.getUnassignedApplicants();
+        console.log(`Number of unassigned applicants after assignment: ${unassignedAfter}`);
+
+        // Log difference
+        console.log(`Number of applicants assigned: ${unassignedBefore - unassignedAfter}`);
+
+        callback();
+    } catch (error) {
+        console.error(error);
+        callback(error);
+    }
+};
