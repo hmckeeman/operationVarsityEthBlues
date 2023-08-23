8797add42d7396b9cec849c15d0ebfc4d260bcd1
hmckeeman <hmckeeman00@gmail.com>
2023-08-23 14:02:24 -0300

    added to the assignOfficers contract so that it now prints out which applicants have been assigned to which officers

--- scripts/assignOfficers.js
+++ scripts/assignOfficers.js
@@ -2,7 +2,6 @@ const Admissions = artifacts.require("Admissions");
 
 module.exports = async function(callback) {
     try {
-        // Fetch the deployed Admissions contract instance
         const admissions = await Admissions.deployed();
 
         // Check the number of unassigned applicants before the assignment
@@ -19,13 +18,22 @@ module.exports = async function(callback) {
         // Log difference
         console.log(`Number of applicants assigned: ${unassignedBefore - unassignedAfter}`);
 
+        // Get list of approved officers
+        const {1: officers} = await admissions.getAdmissionsOfficers();
+
+        // For each officer, fetch and display their assigned applicants
+        for (const officer of officers) {
+            const {0: officerApplicants, 1: count} = await admissions.getApplicantsForOfficer(officer);
+            console.log(`Officer ${officer} has ${count} assigned applicants:`);
+            officerApplicants.forEach(applicant => {
+                console.log(`- ${applicant}`);
+            });
+            console.log("----------");  // Separate the lists for readability
+        }
+
         callback();
     } catch (error) {
-        console.error(error);
+        console.error("Error assigning officers or fetching data:", error);
         callback(error);
     }
 };
-
-
-
-//truffle exec scripts/assignOfficers.js
