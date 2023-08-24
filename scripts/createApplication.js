8fdea9e27cc6711868d1b7f98c61cc2376dfc401
hmckeeman <hmckeeman00@gmail.com>
2023-08-24 12:13:47 -0300

    fixed bug in the createApplication.js script (i forgot to fetch the applicantData) also made the console logs prettier and more readable

--- scripts/createApplication.js
+++ scripts/createApplication.js
@@ -14,24 +14,25 @@ module.exports = async function(callback) {
         // Calling the function to create the application
         await applicationInstance.createApplication(applicantName, universityName, ipfsLink);
         
-        // Fetching the recently created token ID (since we know the `_tokenIds` counter is incremented after minting, 
-        // the most recent token ID will be the current value minus one)
-        const tokenId = (await applicationInstance.totalSupply()).sub(new web3.utils.BN(1));
+        // Fetching the recently created token ID 
+        const tokenIdBN = await applicationInstance.getCurrentTokenId();
+        const tokenId = tokenIdBN.toNumber() > 0 ? tokenIdBN.sub(new web3.utils.BN(1)) : tokenIdBN;
 
         // Fetching the application details using the token ID
-        const applicationData = await applicationInstance.getApplication(tokenId);
+        const applicationData = await applicationInstance.getApplication(tokenId); // This line was missing
 
-        // Displaying the information
+        const admissionsAddress = await applicationInstance.getAdmissionsContractAddress();
+        const applicantAddress = await applicationInstance.getApplicantContractAddress();
+
         console.log("\n-----------------------------");
         console.log("Application Contract Address:", applicationInstance.address);
+        console.log("Admissions Contract Address:", admissionsAddress);
+        console.log("Applicant Contract Address:", applicantAddress);
         console.log("Applicant Name:", applicationData.name);
         console.log("University Name:", applicationData.university);
         console.log("IPFS Link:", applicationData.ipfsLink);
         console.log("Token ID of Application:", tokenId.toString());
         console.log("-----------------------------\n");
-
-        console.log("Application deployed!");
-
         callback();
     } catch (error) {
         console.error("Error deploying application:", error);
