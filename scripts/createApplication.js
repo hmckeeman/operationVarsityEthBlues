0d5fcaaa3a5729b5b77c73df1326670298f77d5d
hmckeeman <hmckeeman00@gmail.com>
2023-08-24 11:36:16 -0300

    added console logs in the createApplication.js script to help with debugging

--- scripts/createApplication.js
+++ scripts/createApplication.js
@@ -10,7 +10,26 @@ module.exports = async function(callback) {
         const applicationInstance = await Application.deployed();
 
         console.log("Deploying application...");
+
+        // Calling the function to create the application
         await applicationInstance.createApplication(applicantName, universityName, ipfsLink);
+        
+        // Fetching the recently created token ID (since we know the `_tokenIds` counter is incremented after minting, 
+        // the most recent token ID will be the current value minus one)
+        const tokenId = (await applicationInstance.totalSupply()).sub(new web3.utils.BN(1));
+
+        // Fetching the application details using the token ID
+        const applicationData = await applicationInstance.getApplication(tokenId);
+
+        // Displaying the information
+        console.log("\n-----------------------------");
+        console.log("Application Contract Address:", applicationInstance.address);
+        console.log("Applicant Name:", applicationData.name);
+        console.log("University Name:", applicationData.university);
+        console.log("IPFS Link:", applicationData.ipfsLink);
+        console.log("Token ID of Application:", tokenId.toString());
+        console.log("-----------------------------\n");
+
         console.log("Application deployed!");
 
         callback();
