c1ba993344eb5e673c43c381a9631c40c164e037
hmckeeman <hmckeeman00@gmail.com>
2023-08-28 14:38:36 -0300

    fixed bug in the way applicants were being fetched from the officer contract

--- scripts/viewApplication.js
+++ scripts/viewApplication.js
@@ -1,37 +1,43 @@
 const Officer = artifacts.require("Officer");
+const Admissions = artifacts.require("Admissions");
 
 module.exports = async function(callback) {
     try {
-        const accounts = await web3.eth.getAccounts();
-        const officerAccount = accounts[0]; // adjust this if the officer is using a different account
-
         // Instantiate the Officer contract
         const officerInstance = await Officer.deployed();
-        console.log("Officer Contract Address:", officerInstance.address);
+        console.log("\nOfficer Contract Address:", officerInstance.address);
 
-        // This assumes that you want to view the application for the first applicant.
-        // You may want to adjust this if you're viewing a different applicant.
-        // Remember, the "applicantContract" here refers to the address of the specific applicant.
-        const applicantContract = accounts[1]; // adjust this to the appropriate applicant's contract address
+        // Instantiate the Admissions contract
+        const admissionsInstance = await Admissions.deployed();
+        console.log("\nAdmissions Contract Address:", admissionsInstance.address);
 
-        console.log("Fetching application for Applicant Address:", applicantContract);
+        // Fetch the assigned applicants from the Admissions contract
+        const result = await admissionsInstance.getApplicantsForOfficer(officerInstance.address);
+        const assignedFromAdmissions = result['0'];
+        console.log("\nAssigned applicants from Admissions:", assignedFromAdmissions);
 
-        // Use the viewApplicant function
-        const [name, university, ipfsLink, decisionMade, decision] = await officerInstance.viewApplicant(applicantContract);
+        if (!assignedFromAdmissions || assignedFromAdmissions.length === 0) {
+            console.log("\nNo applicants assigned from Admissions contract.");
+            return; // If there are no assigned applicants, we can exit early.
+        }
 
-        // Display the applicant's data
-        console.log("\nApplicant Data:");
-        console.log("Name:", name);
-        console.log("University:", university);
-        console.log("IPFS Link:", ipfsLink);
-        if(decisionMade) {
-            console.log("Decision:", decision);
-        } else {
-            console.log("Decision has not been made yet.");
+        // Display the data for each assigned applicant
+        console.log("\nFetching data for assigned applicants...");
+        for (let applicantAddress of assignedFromAdmissions) {
+            const [name, university, ipfsLink, decisionMade, decision] = await officerInstance.viewApplicant(applicantAddress);
+            console.log(`\nApplicant Address: ${applicantAddress}`);
+            console.log(`Name: ${name}`);
+            console.log(`University: ${university}`);
+            console.log(`IPFS Link: ${ipfsLink}`);
+            if (decisionMade) {
+                console.log(`Decision: ${decision}`);
+            } else {
+                console.log("Decision not made yet.");
+            }
         }
 
     } catch (error) {
-        console.error("Error encountered:", error);
+        console.error("\nError encountered:", error);
     }
 
     callback();
