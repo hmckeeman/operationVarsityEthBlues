08cf50509cd99ca2b7bd011c5bd2bea055e1015b
hmckeeman <hmckeeman00@gmail.com>
2023-08-31 10:54:56 -0300

    updated to the approveApplicant.js script to utilize the new json file - will be working on the applicant json files tomorrow

--- scripts/approveApplicant.js
+++ scripts/approveApplicant.js
@@ -1,20 +1,31 @@
-const OfficerContract = artifacts.require("Officer");
+const fs = require('fs');
 
 module.exports = async function(callback) {
-    try {
-        // Initialize contract
-        const officerContract = await OfficerContract.at("Your_Officier_Contract_Address_Here");
+  try {
+    // Use Truffle's native "artifacts" object to get the deployed contract instances
+    const Officer = artifacts.require("Officer");
 
-        // Accept applicant
-        const applicantAddress = "Applicant_Address_Here";
-        await officerContract.acceptApplicant(applicantAddress);
+    // Get a list of all available accounts
+    const accounts = await web3.eth.getAccounts();
 
-        // View application details if needed
-        const applicationDetails = await officerContract.viewApplicant(applicantAddress);
-        console.log("Application details:", applicationDetails);
+    // Read the deployed Officer addresses from the JSON file
+    const deployedOfficerAddresses = JSON.parse(fs.readFileSync('deployedOfficerAddresses.json', 'utf8'));
 
-    } catch (error) {
-        console.error(error);
-    }
-    callback();
+    // Use the first Officer address as an example
+    const officerAddress = deployedOfficerAddresses[0];
+
+    // Use Truffle's "at" function to get an instance of the deployed contract at the specific address
+    const officerInstance = await Officer.at(officerAddress);
+
+    const applicantAddress = "0xYourApplicantContractAddressHere"; // Replace this with the actual address
+
+    // Invoke the "acceptApplicant" method of the deployed Officer contract
+    const txReceipt = await officerInstance.acceptApplicant(applicantAddress, { from: accounts[0] });
+    console.log("Transaction receipt: ", txReceipt);
+    
+    callback(); // Exit the script successfully
+  } catch (error) {
+    console.error("Error in approving applicant: ", error);
+    callback(error); // Exit the script with an error state
+  }
 };
