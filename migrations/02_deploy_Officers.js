d55a3eaadfe0fa5be3560aa0878ac450bdd410ed
hmckeeman <hmckeeman00@gmail.com>
2023-08-31 10:48:24 -0300

    created new 02_deploy_Officers script which keeps track of the truffle addresses in a json file for scripts

--- migrations/02_deploy_Officers.js
+++ migrations/02_deploy_Officers.js
@@ -1,50 +1,21 @@
-const Web3 = require('web3');
+const fs = require('fs');
 const Admissions = artifacts.require("Admissions");
 const Officer = artifacts.require("Officer");
 
 module.exports = async function(deployer, network, accounts) {
-  const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545'); // Replace with your web3 provider if different
   const deployerAddress = accounts[0];
-  
-  // Deploy the Admissions contract first
-  console.log("Deploying Admissions contract from:", deployerAddress);
-  console.log("Deploying with maxStudents:", 1000);
-  
-  let admissionsInstance;
-  try {
-    await deployer.deploy(Admissions, 1000, { from: deployerAddress });
-    admissionsInstance = await Admissions.deployed();
-    console.log("Admissions Deployment successful");
-  } catch (error) {
-    console.log("Admissions Deployment failed:", error);
-    return;
-  }
-  
-  // Define the number of Officer contracts you want to deploy
   const numberOfOfficers = 3;
+  const deployedOfficerAddresses = []; // Array to hold deployed Officer addresses
   
-  for (let i = 0; i < numberOfOfficers; i++) {
-    try {
-      // Deploy each Officer contract instance
-      const officerDeployResult = await deployer.deploy(Officer, admissionsInstance.address, { from: deployerAddress });
-      const officerInstance = await Officer.deployed();
-      
-      console.log(`Officer ${i + 1} Deployment successful`);
+  await deployer.deploy(Admissions, 1000, { from: deployerAddress });
+  const admissionsInstance = await Admissions.deployed();
 
-      // Listen to the OfficerDeployed event
-      const officerDeployedEvent = officerInstance.contract.getPastEvents("OfficerDeployed", {
-        fromBlock: 0,
-        toBlock: "latest"
-      }, (error, events) => {
-        if (error) console.error('Error in OfficerDeployed event:', error);
-        else {
-          // Do something with the event data, e.g., log or store it
-          console.log('Received OfficerDeployed event:', events);
-        }
-      });
-      
-    } catch (error) {
-      console.log(`Officer ${i + 1} Deployment failed:`, error);
-    }
+  for (let i = 0; i < numberOfOfficers; i++) {
+    await deployer.deploy(Officer, admissionsInstance.address, { from: deployerAddress });
+    const officerInstance = await Officer.deployed();
+    deployedOfficerAddresses.push(officerInstance.address); // Add deployed Officer address to the array
   }
+
+  // Write the deployed Officer addresses to a JSON file
+  fs.writeFileSync('deployedOfficerAddresses.json', JSON.stringify(deployedOfficerAddresses));
 };
