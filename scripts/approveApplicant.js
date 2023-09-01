b18a4af307bfd6c6df2ca614859085a3866856cb
hmckeeman <hmckeeman00@gmail.com>
2023-09-01 14:44:13 -0300

    updated the approveApplicant script to use the json file from deployment

--- scripts/approveApplicant.js
+++ scripts/approveApplicant.js
@@ -1,31 +1,43 @@
 const fs = require('fs');
 
 module.exports = async function(callback) {
-  try {
-    // Use Truffle's native "artifacts" object to get the deployed contract instances
-    const Officer = artifacts.require("Officer");
+    try {
+        console.log("Starting to approve applicants.");
 
-    // Get a list of all available accounts
-    const accounts = await web3.eth.getAccounts();
+        // Use Truffle's native "artifacts" object to get the deployed contract instances
+        const Officer = artifacts.require("Officer");
 
-    // Read the deployed Officer addresses from the JSON file
-    const deployedOfficerAddresses = JSON.parse(fs.readFileSync('deployedOfficerAddresses.json', 'utf8'));
+        // Get a list of all available accounts
+        const accounts = await web3.eth.getAccounts();
+        console.log("Accounts: ", accounts);
 
-    // Use the first Officer address as an example
-    const officerAddress = deployedOfficerAddresses[0];
+        // Read the deployed Officer addresses from the JSON file
+        const deployedOfficerAddresses = JSON.parse(fs.readFileSync('deployedOfficerAddresses.json', 'utf8'));
+        console.log("Deployed Officer Addresses: ", deployedOfficerAddresses);
 
-    // Use Truffle's "at" function to get an instance of the deployed contract at the specific address
-    const officerInstance = await Officer.at(officerAddress);
+        // Read the deployed Applicant addresses from the JSON file
+        const deployedApplicantAddresses = JSON.parse(fs.readFileSync('deployedApplicantAddresses.json', 'utf8'));
+        console.log("Deployed Applicant Addresses: ", deployedApplicantAddresses);
 
-    const applicantAddress = "0xYourApplicantContractAddressHere"; // Replace this with the actual address
+        // Use the first Officer address as an example
+        const officerAddress = deployedOfficerAddresses[0];
+        console.log("Using Officer at address: ", officerAddress);
 
-    // Invoke the "acceptApplicant" method of the deployed Officer contract
-    const txReceipt = await officerInstance.acceptApplicant(applicantAddress, { from: accounts[0] });
-    console.log("Transaction receipt: ", txReceipt);
-    
-    callback(); // Exit the script successfully
-  } catch (error) {
-    console.error("Error in approving applicant: ", error);
-    callback(error); // Exit the script with an error state
-  }
+        // Use Truffle's "at" function to get an instance of the deployed contract at the specific address
+        const officerInstance = await Officer.at(officerAddress);
+        console.log("Officer instance: ", officerInstance);
+
+        for (const applicantAddress of deployedApplicantAddresses) {
+            // Invoke the "acceptApplicant" method of the deployed Officer contract
+            console.log(`Approving applicant at address ${applicantAddress}...`);
+            const txReceipt = await officerInstance.acceptApplicant(applicantAddress, { from: accounts[0] });
+            console.log(`Transaction receipt for Applicant ${applicantAddress}: `, txReceipt);
+        }
+
+        console.log("Finished approving applicants.");
+        callback(); // Exit the script successfully
+    } catch (error) {
+        console.error("Error in approving applicant: ", error);
+        callback(error); // Exit the script with an error state
+    }
 };
