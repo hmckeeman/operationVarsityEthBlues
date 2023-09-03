3b3abef256ae002bfc7bd2a5babe6b3e6d2710a2
hmckeeman <hmckeeman00@gmail.com>
2023-09-03 12:25:13 -0300

    changed the officer deployment script so it now deploys the contracts from different accounts

--- migrations/02_deploy_Officers.js
+++ migrations/02_deploy_Officers.js
@@ -4,14 +4,20 @@ const Officer = artifacts.require("Officer");
 
 module.exports = async function(deployer, network, accounts) {
   const deployerAddress = accounts[0];
-  const numberOfOfficers = 3;
+  const numberOfOfficers = 3; // This should be less than or equal to accounts.length - 1
+
+  if (numberOfOfficers >= accounts.length) {
+    throw new Error("Not enough accounts to deploy officers");
+  }
+
   const deployedOfficerAddresses = []; // Array to hold deployed Officer addresses
-  
+
   await deployer.deploy(Admissions, 1000, { from: deployerAddress });
   const admissionsInstance = await Admissions.deployed();
 
   for (let i = 0; i < numberOfOfficers; i++) {
-    await deployer.deploy(Officer, admissionsInstance.address, { from: deployerAddress });
+    const officerAccount = accounts[i + 1]; // Use a different account for each Officer
+    await deployer.deploy(Officer, admissionsInstance.address, { from: officerAccount });
     const officerInstance = await Officer.deployed();
     deployedOfficerAddresses.push(officerInstance.address); // Add deployed Officer address to the array
   }
