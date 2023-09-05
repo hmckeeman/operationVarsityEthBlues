49e8ca64b347dd026371712f3b67ce13e5edda1a
hmckeeman <hmckeeman00@gmail.com>
2023-09-05 12:14:11 -0300

    fixed 03_deploy_Applicant.js to clear the addresses of the JSON file with new migrations

--- migrations/03_deploy_Applicant.js
+++ migrations/03_deploy_Applicant.js
@@ -1,6 +1,6 @@
 const Admissions = artifacts.require("Admissions");
 const Applicant = artifacts.require("Applicant");
-const fs = require('fs');  // Add this line to include the File System module
+const fs = require('fs');  // Include the File System module
 
 module.exports = async function(deployer, network, accounts) {
   // Fetch the deployed Admissions contract
@@ -9,16 +9,21 @@ module.exports = async function(deployer, network, accounts) {
   // Number of instances you want to deploy
   const numberOfInstances = 4;
 
-  // An array to keep track of the deployed addresses
+  // Initialize a fresh array to keep track of the deployed addresses
   let deployedAddresses = [];
 
   for (let i = 0; i < numberOfInstances; i++) {
     // Deploy the Applicant contract and pass the address of the Admissions contract as a constructor parameter
     await deployer.deploy(Applicant, admissions.address);
+    
+    // Push the newly deployed address into the array
     deployedAddresses.push(Applicant.address);
 
     console.log(`Applicant ${i + 1} deployed with the address:`, Applicant.address);
   }
+
+  // Log the deployed addresses before saving, just for double-checking
+  console.log("Deployed addresses before saving:", deployedAddresses);
 
   // Save the deployed addresses to a JSON file
   fs.writeFileSync('deployedApplicantAddresses.json', JSON.stringify(deployedAddresses, null, 2));
