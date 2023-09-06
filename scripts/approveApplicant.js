5c85f94569cd045852dee07ba97323968b18b8c8
hmckeeman <hmckeeman00@gmail.com>
2023-09-06 14:10:55 -0300

    cleaned up the approveApplicant.js script so it now only prints out the human readable part

--- scripts/approveApplicant.js
+++ scripts/approveApplicant.js
@@ -2,51 +2,46 @@ const fs = require('fs');
 
 module.exports = async function(callback) {
     try {
-        // Use Truffle's native "artifacts" object to get the deployed contract instances
         const Officer = artifacts.require("Officer");
-
-        // Get a list of all available accounts
         const accounts = await web3.eth.getAccounts();
-        console.log("Accounts: ", accounts);
 
-        // Read the deployed Officer addresses from the JSON file
+        // Read only the necessary addresses from JSON files
         const deployedOfficerAddresses = JSON.parse(fs.readFileSync('deployedOfficerAddresses.json', 'utf8'));
-        console.log("Deployed Officer Addresses: ", deployedOfficerAddresses);
-
-        // Read the deployed Applicant addresses from the JSON file
         const deployedApplicantAddresses = JSON.parse(fs.readFileSync('deployedApplicantAddresses.json', 'utf8'));
-        console.log("Deployed Applicant Addresses: ", deployedApplicantAddresses);
 
-        // Using the specified Officer address
-        const officerAddress = "0x875625D126619c6842B6e10f3EaDdCa357110759";  // You can read this dynamically too, if needed
-        console.log("Using Officer at address: ", officerAddress);
+        const officerAddress = "0x875625D126619c6842B6e10f3EaDdCa357110759";  
+        const specificApplicantAddress = deployedApplicantAddresses[deployedApplicantAddresses.length - 1];
+        const deployerAccount = "0x93a2109C4C2AeE626722CCEC9138929d94774407";
 
-        // Use the specific applicant address you are interested in
-        const specificApplicantAddress = deployedApplicantAddresses[deployedApplicantAddresses.length - 1];  // Taking the last address in this case
-        console.log("Approving specific applicant at address: ", specificApplicantAddress);
-
-        // Search for the deployerAccount in the available accounts
-        const deployerAccount = "0x93a2109C4C2AeE626722CCEC9138929d94774407";  // You can read this dynamically too, if needed
         const accountToUse = accounts.find(account => account.toLowerCase() === deployerAccount.toLowerCase());
 
         if (!accountToUse) {
             throw new Error(`Deployer account ${deployerAccount} not found among available accounts`);
         }
-        console.log(`Using deployer account: ${accountToUse}`);
 
-        // Use Truffle's "at" function to get an instance of the deployed contract at the specific address
         const officerInstance = await Officer.at(officerAddress);
-        console.log("Officer instance: ", officerInstance);
 
-        // Approve the specific applicant
-        console.log(`Approving applicant at address ${specificApplicantAddress}...`);
+        console.log("========= Approve Applicant Script =========");
+        console.log(`Officer Address: ${officerAddress}`);
+        console.log(`Applicant Address: ${specificApplicantAddress}`);
+        console.log(`Deployer Account: ${accountToUse}`);
+        console.log("===========================================");
+
+        console.log(`\nApproving applicant at address ${specificApplicantAddress}...`);
+
         const txReceipt = await officerInstance.acceptApplicant(specificApplicantAddress, { from: accountToUse });
-        console.log(`Transaction receipt for Applicant ${specificApplicantAddress}: `, txReceipt);
 
-        console.log("Finished approving applicant.");
-        callback();  // Exit the script successfully
+        console.log("\n========== Transaction Details ============");
+        console.log(`Transaction Hash: ${txReceipt.tx}`);
+        console.log(`Block Number: ${txReceipt.receipt.blockNumber}`);
+        console.log(`Gas Used: ${txReceipt.receipt.gasUsed}`);
+        console.log(`Status: ${txReceipt.receipt.status ? 'Successful' : 'Failed'}`);
+        console.log("==========================================");
+
+        console.log("\nFinished approving applicant.");
+        callback();
     } catch (error) {
         console.error("Error in approving applicant: ", error);
-        callback(error);  // Exit the script with an error state
+        callback(error);
     }
 };
