04fee3d639860fae2adc671aa150c7fbdfe61217
hmckeeman <hmckeeman00@gmail.com>
2023-09-07 13:12:59 -0300

    fixed bug in fetchAcceptedApplicants.js script and cleaned up the console logs

--- scripts/fetchAcceptedApplicants.js
+++ scripts/fetchAcceptedApplicants.js
@@ -1,33 +1,41 @@
 const Admissions = artifacts.require("Admissions");
+const fs = require('fs');
 
 module.exports = async function(callback) {
     try {
-        // Fetch the accounts from the Ethereum node
+        // Fetch the accounts from the Ethereum node (even though it's not being used in the current context)
         const accounts = await web3.eth.getAccounts();
 
+        console.log("==============================================");
+        console.log("FETCHING ACCEPTED APPLICANTS FROM ADMISSIONS CONTRACT");
+        console.log("==============================================\n");
+
         // Fetch the deployed Admissions contract instance
-        console.log("Fetching Admissions contract instance...");
+        console.log("[1/3] Fetching Admissions contract instance...");
         const admissionsInstance = await Admissions.deployed();
-        console.log("Fetched. Admissions Contract Address:", admissionsInstance.address);
+        console.log(`[SUCCESS] Admissions Contract Address: ${admissionsInstance.address}\n`);
 
         // Fetch accepted applicants
-        console.log("Fetching accepted applicants...");
+        console.log("[2/3] Fetching accepted applicants...");
         const result = await admissionsInstance.getAcceptedApplicants();
-        console.log("Result from getAcceptedApplicants: ", result);
-
         const acceptedApplicantCount = result['0'].toNumber(); // Converting BN to a regular number
         const acceptedApplicantAddresses = result['1'];
-
-        console.log(`Number of accepted applicants: ${acceptedApplicantCount}`);
-        console.log("Accepted applicant addresses: ", acceptedApplicantAddresses);
+        console.log(`[SUCCESS] Total Accepted Applicants: ${acceptedApplicantCount}`);
+        console.log("Accepted Applicant Addresses:", acceptedApplicantAddresses.join(", "), "\n");
 
         // If you still wish to save to a file, you can do it here.
+        console.log("[3/3] Saving accepted applicant addresses to JSON file...");
         fs.writeFileSync('acceptedApplicantAddresses.json', JSON.stringify(acceptedApplicantAddresses));
+        console.log("[SUCCESS] Data saved to acceptedApplicantAddresses.json.\n");
+
+        console.log("==============================================");
+        console.log("SCRIPT EXECUTION COMPLETE");
+        console.log("==============================================");
 
         callback();  // Exit the script successfully
 
     } catch (error) {
-        console.error("Error fetching accepted applicants: ", error);
+        console.error("[ERROR] Fetching accepted applicants:", error);
         callback(error);  // Exit the script with an error state
     }
 };
