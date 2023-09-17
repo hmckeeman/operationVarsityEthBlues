f45ac98ee67d1be7715a7b6ed48ca7b5c228a022
hmckeeman <hmckeeman00@gmail.com>
2023-09-17 12:46:52 -0300

    added checkAssignment script to the unversityScript folder in place of the test

---
+++ scripts/universityScripts/checkAssignment.js
@@ -0,0 +1,34 @@
+const AdmissionsContract = artifacts.require("Admissions");
+
+module.exports = async (callback) => {
+  try {
+    console.log("Starting the script...");
+
+    // Load contract instance
+    console.log("Loading Admissions contract instance...");
+    const admissionsInstance = await AdmissionsContract.deployed();
+    console.log("Contract instance loaded successfully.");
+
+    // Use your logic to fetch officer assignments, etc.
+    console.log("Fetching assigned applicants...");
+    const result = await admissionsInstance.getAssignedApplicants(); // Replace with your actual function
+    
+    const numberOfApplicants = result[0].toNumber();
+    const assignedOfficers = result[1];
+
+    console.log(`Total number of applicants: ${numberOfApplicants}`);
+    console.log("Assigned officers:", assignedOfficers);
+
+    if (numberOfApplicants === assignedOfficers.length) {
+      console.log("All applicants have been successfully assigned an officer.");
+    } else {
+      console.error(`Mismatch: Expected ${numberOfApplicants} assignments but got ${assignedOfficers.length}.`);
+    }
+
+    console.log("Script execution completed.");
+
+  } catch (error) {
+    console.error("Error encountered:", error);
+  }
+  callback();
+};
