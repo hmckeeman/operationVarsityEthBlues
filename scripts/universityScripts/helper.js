b1372cea961a212dd499029a7fafee244301d900
hmckeeman <hmckeeman00@gmail.com>
2023-09-24 11:58:34 -0300

    deleted help.js file from old architecture

--- scripts/universityScripts/helper.js
+++
@@ -1,8 +0,0 @@
-const Admissions = artifacts.require("Admissions"); // Adjust the contract name if necessary
-const accounts = await web3.eth.getAccounts();
-
-const maxStudents = 1000; // Replace with your desired initial value
-const admissionsInstance = await Admissions.new(maxStudents, { from: accounts[0] });
-
-const unassignedApplicantsCount = await admissionsInstance.getUnassignedApplicantsCount();
-console.log("Unassigned Applicants Count:", unassignedApplicantsCount.toNumber());
