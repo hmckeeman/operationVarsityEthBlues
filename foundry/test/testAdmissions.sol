4eaa189f57ca0c89f754d3e699a9bf3a007312da
hmckeeman <hmckeeman00@gmail.com>
2023-10-05 11:33:20 -0300

    created testAdmissions.sol in the test folder for foundry project

---
+++ foundry/test/testAdmissions.sol
@@ -0,0 +1,26 @@
+// Import necessary dependencies for writing tests
+import "@foundry-sh/core";
+import "hardhat/console.sol"; // Import console for debugging
+
+// Import the contract you want to test
+import "./Admissions.sol";
+
+contract AdmissionsTest {
+    Admissions admissions;
+
+    // Deploy the contract before running tests
+    function beforeEach() public {
+        admissions = deploy(Admissions, 100); // Deploy the Admissions contract with a maximum of 100 students
+    }
+
+    // Write a simple test case
+    function testRegisterOfficer() public {
+        // Call the registerOfficer function from the Admissions contract
+        // Note: You should have the logic to approve the sender as an officer
+        admissions.registerOfficer(address(this));
+
+        // Check if the sender has been registered as an officer
+        bool isOfficer = admissions.isAdmissionsOfficer(address(this));
+        assertTrue(isOfficer, "Should be a registered officer");
+    }
+}
