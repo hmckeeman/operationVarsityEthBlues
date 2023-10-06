653a7eb5d68cba1ca20d5e0d3b8cf4aa52b5da2f
hmckeeman <hmckeeman00@gmail.com>
2023-10-06 13:33:39 -0300

    installed forge std- for testing purposes

--- foundry/test/testAdmissions.sol
+++ foundry/test/testAdmissions.sol
@@ -1,26 +1,19 @@
-// Import necessary dependencies for writing tests
-//import "@foundry-sh/core";
-//import "hardhat/console.sol"; // Import console for debugging
-pragma solidity ^0.8.17;
-// Import the contract you want to test
-import "../src/admissions.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract AdmissionsTest {
+import "node_modules/forge-std/Test.sol";
+import "../src/admissions.sol"; // Update the import path to your Admissions contract
+
+contract AdmissionsTest is Test {
+
     Admissions admissions;
 
-    // Deploy the contract before running tests
-    function beforeEach() public {
-        admissions = deploy(Admissions, 100); // Deploy the Admissions contract with a maximum of 100 students
+    function setUp() public {
+        admissions = deployContract(Admissions, 100); // Deploy the Admissions contract with a maximum of 100 students
     }
 
-    // Write a simple test case
-    function testRegisterOfficer() public {
-        // Call the registerOfficer function from the Admissions contract
-        // Note: You should have the logic to approve the sender as an officer
-        admissions.registerOfficer(address(this));
-
-        // Check if the sender has been registered as an officer
-        bool isOfficer = admissions.isAdmissionsOfficer(address(this));
+    function testNameIsSpacebear() public {
+        bool isOfficer = admissions.isAdmissionsOfficer(msg.sender);
         assertTrue(isOfficer, "Should be a registered officer");
     }
 }
