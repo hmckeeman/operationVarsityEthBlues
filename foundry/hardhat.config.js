b1c551e770d4118ced3cb91f78ed669d01f0e9de
hmckeeman <hmckeeman00@gmail.com>
2023-10-12 14:46:47 -0300

    added to my hardhat.config.js file

---
+++ foundry/hardhat.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    solidity: "0.8.17",
+    paths: {
+      "@openzeppelin": "./node_modules/@openzeppelin",
+      "forge-std": "./node_modules/forge-std",
+      "forge-std/src/": "./node_modules/forge-std/src/",
+    },
+  };
+  
